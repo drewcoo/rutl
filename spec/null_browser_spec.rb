3eb2c5fec1bd76807864e068bcb1bc70f7c2d52e
Drew Cooper <drewcoo@gmail.com>
2018-06-20 16:46:47 -0700

    Rejigger browser tests.
    Make it so that we run:
    - same tests for all browsers
    - per browser if desired
    - currently only FF and Chrome; IE up next

---
+++ spec/null_browser_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+require 'rutl/interface/null'
+
+RSpec.describe RUTL::Interface::Null, :fast do
+  let!(:browser) do
+    RUTL::Browser.new(type: :null)
+  end
+
+  let(:browser2) do
+    RUTL::Browser.new(type: :null)
+  end
+
+  before do
+    goto(Page1)
+  end
+
+  it 'click a button and check the return' do
+    result = ok_button.click
+    expect(result).to be_page(Page2)
+  end
+
+  it 'click a link and check the return' do
+    result = ok_link.click
+    expect(result).to be_page(Page1)
+  end
+
+  it 'click a button and check the page' do
+    ok_button.click
+    expect(current_page).to be_page(Page2)
+  end
+
+  it 'click a link and check the page' do
+    ok_link.click
+    expect(current_page).to be_page(Page1)
+  end
+
+  it 'enter some text' do
+    password_text = 'foobarbaz'
+    expect(password_text).to eq 'foobarbaz'
+  end
+
+  it 'do a thing' do
+    password_text = 'am i texting'
+    ok_link.click
+    expect(okay_text.to_s).to eq ''
+  end
+
+  it 'load another page' do
+    goto(Page2)
+    page_init = current_page
+    page_final = belly_button.click
+    expect(page_init).not_to be_instance_of(page_final.class)
+  end
+
+  context 'when click a button' do
+    it 'goes to another page' do
+      ok_button.click
+      expect(current_page).to be_page(Page2)
+    end
+  end
+
+  it 'see url' do
+    expect(current_page.url).to match(/page1/i)
+  end
+
+  context 'with another browser intance' do
+    before do
+      # We have to call this browser by name.
+      # The shortcut assumes we're going to "browser."
+      browser2.goto(Page1)
+    end
+
+    it 'reads and write text' do
+      browser2.goto(Page1)
+      browser2.password_text = 'changeme'
+      expect(browser2.password_text).to eq 'changeme'
+    end
+
+    it 'changes text' do
+      browser2.goto(Page1)
+      browser2.password_text = 'changeme'
+      browser2.password_text = 'changed'
+      expect(browser2.password_text).to eq 'changed'
+    end
+
+    it 'changes multiple fake text fields' do
+      browser2.goto(Page1)
+      browser2.password_text = 'foo'
+      browser2.okay_text = 'bar'
+      expect(browser2.password_text).to eq 'foo'
+    end
+  end
+end
---
+++ spec/web_browser_spec.rb
@@ -0,0 +1,84 @@
+require 'webdrivers' unless ENV['CIRCLECI']
+require 'spec_helper'
+BROWSER_TYPES = [:chrome, :firefox]
+BROWSER_TYPES.each do |browser_type|
+  require "rutl/interface/#{browser_type}"
+  #
+  # There's special magic here in that these tests are tagged slow for all
+  # browsers and there are tagged instances or each of these for the individual
+  # browsers. So you could:
+  #   rpsec --tag slow
+  # or
+  #   rspec --tag firefox
+  # for example.
+  #
+  RSpec.describe "RUTL::Interface::#{browser_type.capitalize}",
+                 browser_type, :slow do
+    let!(:browser) do
+      RUTL::Browser.new(type: browser_type)
+    end
+
+    before do
+      goto(InternetLoginPage)
+    end
+
+    context 'with text field' do
+      it 'read/write' do
+        username = 'foo'
+        username_text = username
+        expect(username_text).to eq username
+      end
+
+      it 'clear' do
+        username = 'foo'
+        username_text = username
+        username_text.clear
+        expect(username_text).to eq ''
+      end
+    end
+
+    context 'with password field' do
+      it 'read' do
+        username = 'foo'
+        username_text = username
+        expect(username_text).to eq username
+      end
+    end
+
+    it 'fails bad login' do
+      unless ENV['CIRCLECI']
+        # CircleCI (Docker?) seems to have probelms with showing the div that
+        # we use to determine error. Skip this on Circle for now.
+        username_text = 'tomsmith'
+        password_text = 'foo'
+        login_button.click
+        # This test site actually has different displayed for invalid user and
+        # password. That is an informations leak thus a bug and I'd rather not
+        # contribute to someone else "testing the bug in" so this only tests for
+        # there being an error and not the type of error.
+        expect(current_page).to be_page(InternetLoginErrorPage)
+      end
+    end
+
+    context 'when log in' do
+      before(:each) do
+        # TODO: browser doesn't have focus and get username_text unless
+        # I explicitly pass browser.username_text here.
+        # Why?
+        browser.username_text = 'tomsmith'
+        browser.password_text = 'SuperSecretPassword!'
+      end
+
+      it 'lands on logged in page' do
+        login_button.click
+        expect(current_page).to be_page(InternetLoggedInPage)
+      end
+
+      it 'can log back out' do
+        login_button.click
+        logout_button.click
+        expect(current_page.url).to eq InternetLoginPage.url
+      end
+    end
+  end
+end
